9822f9fb725b9a86d9657fa2b51d6177c05e4f3d
Alexis Szabo <alexis.szabo@hotmail.com>
2018-10-01 13:55:50 -0700

    Password protect site (without authentication)
    I am using the lockup gem. Temporarily using a forked branch that adds
    support for Rails 5.2 encrypted credentials

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Lockup::Engine, at: '/lockup'
   root 'pages#index'
   post 'address_checker/check', to: 'address_checker#check'
 end
