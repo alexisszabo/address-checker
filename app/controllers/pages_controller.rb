34891753bda10b808c16d3b1329f59a4dedc3e22
Alexis Szabo <alexis.szabo@hotmail.com>
2018-10-01 08:23:30 -0700

    Put some hard-coded view values into controller variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @countries = ['Canada']
+    @provinces = ['British Columnbia']
+    @statuses_default_on = ['New', 'Valid', 'Do not call']
+    @statuses_default_off = ['Moved', 'Duplicate', 'Not valid']
   end
 end
