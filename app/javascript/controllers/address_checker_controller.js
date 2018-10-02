bfd2d3cd5c9096347e7c0cb3857b602c80879303
Alexis Szabo <alexis.szabo@hotmail.com>
2018-10-01 23:30:27 -0700

    Add gui elements for csv export

--- app/javascript/controllers/address_checker_controller.js
+++ app/javascript/controllers/address_checker_controller.js
@@ -1,7 +1,7 @@
 import {Controller} from 'stimulus';
 
 export default class extends Controller {
-  static targets = ["results", "submit"];
+  static targets = ["results", "submit", "kindForeignLanguage", "kindLocal", "purposeImport", "purposeExport"];
 
   beforeSendResults () {
     this.submitTarget.classList.add('is-loading');
@@ -19,6 +19,13 @@ export default class extends Controller {
   displayResults (event) {
     let [data, status, xhr]  = event.detail;
     this.resultsTarget.innerHTML = xhr.response;
+    if (this.kindForeignLanguageTarget.checked) { this.kindIsForeignLanguage(); }
+    if (this.kindLocalTarget.checked)   { this.kindIsLocal(); }
+    // The following may not exist, depending on the ajax return
+    try {
+      if (this.purposeImportTarget.checked) { this.purposeIsImport(); }
+      if (this.purposeExportTarget.checked) { this.purposeIsExport(); }
+    } catch(err) {}
   }
 
   kindIsLocal (event) {
@@ -29,6 +36,16 @@ export default class extends Controller {
   kindIsForeignLanguage (event) {
     this.hideElements(document.getElementsByClassName('local-only'));
     this.showElements(document.getElementsByClassName('foreign-language-only'));
+  }
+
+  purposeIsImport (event) {
+    this.hideElements(document.getElementsByClassName('export-only'));
+    this.showElements(document.getElementsByClassName('import-only'));
+  }
+
+  purposeIsExport (event) {
+    this.hideElements(document.getElementsByClassName('import-only'));
+    this.showElements(document.getElementsByClassName('export-only'));
   }
 
   //Internal Routines
