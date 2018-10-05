a6405a526070d1e0a7a736b19e01b5280880ac28
Alexis Szabo <alexis.szabo@hotmail.com>
2018-10-05 07:54:03 -0700

    Add missing semicolon

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,4 +1,4 @@
-import Choices from 'choices.js'
+import Choices from 'choices.js';
 
 // Place all the behaviors and hooks related to the matching controller here.
 document.addEventListener('turbolinks:load', function () {
