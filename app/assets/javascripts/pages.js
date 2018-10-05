f744853c970f9859206253579b4a670ca2a12ec3
Alexis Szabo <alexis.szabo@hotmail.com>
2018-10-05 07:51:21 -0700

    pages.js: Import choices.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,5 @@
+import Choices from 'choices.js'
+
 // Place all the behaviors and hooks related to the matching controller here.
 document.addEventListener('turbolinks:load', function () {
   new Choices(document.getElementById('language-select'), { removeItemButton: true, paste: false })
